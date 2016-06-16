c196b00feafd85fd94a63273b3a3c06a28f241e2
Luis Villasenor <luis@iceberg9.com>
2016-06-16 10:54:01 -0500

    Se agregan validaciones en campos para no aceptar vacios

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,6 +4,6 @@ class Micropost < ActiveRecord::Base
 	# Un micropost pertenece a un usuario
 	belongs_to :users
 
-	# Valida que el campo 'content' tenga maximo 140 caracteres
-	validates :content, length: { maximum: 140 }
+	# Valida que el campo 'content' tenga maximo 140 caracteres y no este vacio
+	validates :content, length: { maximum: 140 }, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
 	# Asociacion de registros
 	# Un usuario tiene 1 o mas micropost
 	has_many :microposts
+
+	# Valida que no esten vacios los campos
+	validates "El campo no puede estar vacio", presence: true
+	validates "El campo no puede estar vacio", presence: true
+
 end
